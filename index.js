155362f8230d0de0c929624e1f51db806fdb1678
Martha Staus <mjstaus@gmail.com>
2021-11-17 13:24:56 -0800

    added mocha testing

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const { fetchBreedDescription } = require('./breedFetcher');
 // const breedName = process.argv[2];
 
 fetchBreedDescription('Bombay', (err, description) => {
-  if(err){
-    console.log('Error Fetch Details', err)
-    console.log('Status code:', description)
+  if (err) {
+    console.log('Error Fetch Details', err);
+    console.log('Status code:', description);
   } else {
-    console.log(description)
+    console.log(description);
   }
-})
+});
 
 
